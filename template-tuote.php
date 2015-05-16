bb0446b19159aacc5399284dd03f5372e88a9260
Mainoslähde <miika@mainoslahde.fi>
2015-05-16 11:43:31 +0300

    Pikku fixejä

--- template-tuote.php
+++ template-tuote.php
@@ -61,12 +61,6 @@
                             </div>
                         </div>
                     </div>
-                    			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					get_template_part('content-comments'); // COMMENTS     
-				endif;
-			?>
 
                 </section>  
         <?php }
