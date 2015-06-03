fb3f85462e318838158c90ffb0cf6e563246f1f7
Mainoslähde <miika@mainoslahde.fi>
2015-06-03 15:53:00 +0300

    Tuotteen esittämisen päivityksiä

--- single-tuote.php
+++ single-tuote.php
@@ -18,22 +18,39 @@ get_header(); ?>
                             <img class="img-responsive" src="<?php the_field('tuotekuva'); ?>" />
                         </div>  
                         <div class="col-sm-6 col-xs-12">
-                            <img class="img-responsive" src="<?php the_field('tuotteen_nimi'); ?>" />
-                            <p class="kuvaus"><?php the_field('tuotteen_kuvaus'); ?></p>
-                            <span class="tags"><?php the_tags('', ' ', ''); ?></span>
-                            <div class="hinta-wrapper clearfix"><?php the_field('tuotteen_hinta'); ?></div>
+                            <?php if (get_field('nimen_esittaminen') == 'Teksti') : ?>
+                                <h1><?php the_field('tuotteen_nimi'); ?></h1>
+                            <?php else : ?>
+                                <img class="img-responsive" src="<?php the_field('tuotteen_nimi_graafinen'); ?>" />
+                            <?php endif; ?>
+                            <p class="kuvaus">
+                                <?php the_field('tuotteen_kuvaus'); ?>
+                            </p>
+                            <span class="tags"><?php the_terms( $post->ID, 'tayte', '', ' ' ); ?></span>
+                            <div class="hinta-wrapper clearfix">
+                                <?php
+                                // check if the repeater field has rows of data
+                                if( have_rows('annos_koko_wrapper') ):
+                                    // loop through the rows of data
+                                    while ( have_rows('annos_koko_wrapper') ) : the_row(); ?>
+                                        <div class="hinta-inner col-xs-3">
+                                            <span class="kpl">
+                                                <?php the_sub_field('annos_koko'); ?>
+                                            </span>
+                                            <span class="hinta">
+                                                <?php the_sub_field('annoksen_hinta'); ?> €
+                                            </span>
+                                        </div>
+                                        <?php                                      
+                                    endwhile;
+                                else :
+                                    // no rows found
+                                endif;
+                                ?>
+                            </div>
                         </div>
                     </div>
                 </section>  
-
-			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					get_template_part('content-comments'); // COMMENTS     
-				endif;
-			?>
 
 		<?php endwhile; // end of the loop. ?>
 
